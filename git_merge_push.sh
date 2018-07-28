31e11448f7d9d8b6cb7d1543c61b1e23717bd183
seckawijoki@gmail.com <seckawijoki@gmail.com>
2018-07-28 16:36:48 +0800

    changes in merged branch

--- git_merge_push.sh
+++ git_merge_push.sh
@@ -1,44 +1,12 @@
 commitTime=$(date)
 git checkout master
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 2a3de2c8e6dc224edf6c411cb512d5fb7c37da62
 git pull origin master
 git branch mergedBranch
 git branch -a
 git merge mergedBranch
-<<<<<<< HEAD
-=======
-=======
->>>>>>> rebasedBranch
-=======
 git branch mergedBranch
 git branch -a
->>>>>>> 9f868c43b743cafaa31aa882a91b091cc12a6493
->>>>>>> 2a3de2c8e6dc224edf6c411cb512d5fb7c37da62
-git add .
 git commit -m "$commitTime commit with merged branch"
 git push origin master
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> 2a3de2c8e6dc224edf6c411cb512d5fb7c37da62
-git checkout master
-git branch mergedBranch
-git merge mergedBranch
-git push origin mergedBranch
-<<<<<<< HEAD
 
-=======
->>>>>>> rebasedBranch
-=======
-git checkout mergedBranch
-git merge mergedBranch
-git push origin rebasedBranch
-
->>>>>>> 9f868c43b743cafaa31aa882a91b091cc12a6493
->>>>>>> 2a3de2c8e6dc224edf6c411cb512d5fb7c37da62

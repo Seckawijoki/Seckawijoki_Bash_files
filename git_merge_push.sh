aacb025490a8449df2008f417f8cca3543017fb6
seckawijoki@gmail.com <seckawijoki@gmail.com>
2018-07-28 16:01:50 +0800

    commit in merged branch

--- git_merge_push.sh
+++ git_merge_push.sh
@@ -1,21 +1,15 @@
 commitTime=$(date)
 git checkout master
-<<<<<<< HEAD
 git pull origin master
 git branch mergedBranch
 git branch -a
 git merge mergedBranch
-=======
->>>>>>> rebasedBranch
 git add .
 git commit -m "$commitTime commit with merged branch"
 git push origin master
 
-<<<<<<< HEAD
 git checkout master
-
-=======
 git branch mergedBranch
 git merge mergedBranch
 git push origin mergedBranch
->>>>>>> rebasedBranch
+

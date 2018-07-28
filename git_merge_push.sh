b821f6e3b982abca58057d752b22a8e6c1b12bf8
seckawijoki@gmail.com <seckawijoki@gmail.com>
2018-07-28 16:47:10 +0800

    2018年07月28日 16:47:10 commit with merged branch

--- git_merge_push.sh
+++ git_merge_push.sh
@@ -1,1 +1,9 @@
 commitTime=$(date)
+git checkout master
+git merge mergedBranch
+git add .
+git commit -m "$commitTime commit with merged branch"
+git push origin master
+
+git checkout mergedBranch
+git pull origin master
